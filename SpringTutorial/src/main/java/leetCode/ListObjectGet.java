9bad6d5ce8c4d96c1dbfe904508b626c89b101bc
larrylin0914 <roclinya@gmail.com>
2023-08-25 16:05:17 +0800

    no message

--- SpringTutorial/src/main/java/leetCode/ListObjectGet.java
+++ SpringTutorial/src/main/java/leetCode/ListObjectGet.java
@@ -1,7 +1,9 @@
 package leetCode;
 
-import java.util.ArrayList;
+import java.util.*;
 import java.util.stream.Stream;
+
+import org.springframework.util.CollectionUtils;
 
 public class ListObjectGet {
 
@@ -13,18 +15,27 @@ public class ListObjectGet {
 		list.add(obj);
 		list.add(obj2);
 		Stream<ListObject> stream = list.stream();
-//		stream.filter(e -> e != null).forEach(t -> {
-		
-		/* method1 java 8 Stream*/
-			stream.forEach(t -> {
-			System.out.println("結果->>>" + t.getObj1() + " : " + t.getObj2() + " : " + t.getObj3());
+		stream.filter(e -> e != null).forEach(t -> {
+			System.out.println(t);
 		});
+
+		/* method1 java 8 Stream */
+//			stream.forEach(t -> {
+//			System.out.println("結果->>>" + t.getObj1() + " : " + t.getObj2() + " : " + t.getObj3());
+//		});
 		/* method2 */
 //		for(ListObject t :list) {
 //			System.out.println("結果->>>" + t.getObj1() + " : " + t.getObj2() + " : " + t.getObj3());
 //		}
 		
-		
+		//CollectionUtils.containsAny
+		ArrayList<String> list1 = new ArrayList<String>();
+		list1.add("AA");//candidates
+		ArrayList list2 = new ArrayList<String>();
+		list2.addAll(Arrays.asList("AA", "BB"));
+		System.out.println("List2 = " + list2);
+
+		System.out.println("是否有包含:"+ (CollectionUtils.containsAny(list2, list1)==true?"Yes":"No"));//(source,candidates)
 
 	}
 
--- SpringTutorial/src/main/java/leetCode/test.java
+++ SpringTutorial/src/main/java/leetCode/test.java
@@ -2,13 +2,14 @@ package leetCode;
 
 import java.sql.Timestamp;
 import java.util.*;
+import java.util.stream.Collectors;
 
 import org.apache.commons.lang3.ObjectUtils;
 
 public class Test {
 
 	public static void main(String[] args) {
-		
+
 //		   //创建优先队列
 //        PriorityQueue<Integer> numbers = new PriorityQueue<>();
 //        //使用add()方法
@@ -30,47 +31,57 @@ public class Test {
 //        System.out.println("使用poll()删除的元素: " + numberPoll);
 //        
 //        System.out.println("PriorityQueue: " + numbers);
-        
-        String string = "0123456789";
-        byte[] bytes = string.getBytes();
-        
-        String result = new String(bytes,7,2).trim();
-        System.out.println(result);
-        
-//        string.length()
-        System.out.println(string.substring(9, 10));        
-        
-        
-        String string2 ="1,2,3,4";
-        String[] splitResult = string2.split(",");
-        System.out.println(splitResult[2]);
-        splitResult[2].replace("-1" ,"");
-        
-        System.out.println(new Timestamp(System.currentTimeMillis()));
-        System.out.println(Timestamp.valueOf("2023-01-01 11:11:22"));
-        
-        HashMap<String, String> map = new HashMap<String,String>();
-        map.put("123", "test");
-        map.put("123", "test2");
-        map.put("empty", "");
-        map.put("Null", null);
-        System.out.println(map);
-        
-        HashMap<String, Object> mapStringOb  = new HashMap<String,Object>();
-        mapStringOb.put("456", "test3");
-        mapStringOb.put("empty", "");
-        mapStringOb.put("Null", null);
-        System.out.println(mapStringOb);
-        
-        // main thread
-        Thread currThread = Thread.currentThread();
-        
-     
-        System.out.println("current thread = " + currThread);
-        
-        ArrayList<String> arrayList = new ArrayList<String>();
-        System.out.println(ObjectUtils.isEmpty(arrayList));
-        
-	    }
-	}
 
+		String string = "0123456789";
+		byte[] bytes = string.getBytes();
+
+		String result = new String(bytes, 7, 2).trim();
+		System.out.println(result);
+
+//        string.length()
+		System.out.println(string.substring(9, 10));
+
+		String string2 = "1,2,3,4";
+		String[] splitResult = string2.split(",");
+		System.out.println(splitResult[2]);
+		splitResult[2].replace("-1", "");
+
+		System.out.println(new Timestamp(System.currentTimeMillis()));
+		System.out.println(Timestamp.valueOf("2023-01-01 11:11:22"));
+
+		HashMap<String, String> map = new HashMap<String, String>();
+		map.put("123", "test");
+		map.put("123", "test2");
+		map.put("empty", "");
+		map.put("Null", null);
+		System.out.println(map);
+
+		HashMap<String, Object> mapStringOb = new HashMap<String, Object>();
+		mapStringOb.put("456", "test3");
+		mapStringOb.put("empty", "");
+		mapStringOb.put("Null", null);
+		System.out.println(mapStringOb);
+
+		// main thread
+		Thread currThread = Thread.currentThread();
+
+		System.out.println("current thread = " + currThread);
+
+		ArrayList<String> arrayList = new ArrayList<String>();
+		System.out.println(ObjectUtils.isEmpty(arrayList));
+
+		ArrayList<Map<String, String>> arrayMapList = new ArrayList<Map<String, String>>();
+		HashMap<String, String> arrayMap = new HashMap<String, String>();
+		arrayMap.put("a1", "aa");
+		arrayMap.put("b1", "bb");
+		arrayMapList.add(arrayMap);
+		HashMap[] mapToArrayList = new HashMap[arrayMapList.size()];
+		System.out.println(mapToArrayList);
+		arrayMapList.toArray(mapToArrayList);
+		System.out.println(arrayMapList);
+		List resultArrayMapList = arrayMapList.stream().map(e -> new ArrayList<String>(e.values()))
+				.flatMap(List::stream).collect(Collectors.toList());
+		System.out.println(resultArrayMapList);
+
+	}
+}
